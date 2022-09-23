8e39f227fd06b086fcf43b3da6ec954ea5996c95
Samuel Silva <samuel0100wanderson@gmail.com>
2022-09-23 12:14:28 -0300

    Aprendendo C & C++

---
+++ 01 Inicio.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <stdio.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char** argv) 
+	{
+	int idade = 19;
+	float altura_gato = 30.08;
+	char vitamina = 'C';
+	printf("Eu tenho %d anos de idade \n", idade);
+	printf("Meu gato tem %.2f centimetros\n", altura_gato);
+	printf("Laranja e rica em vitamina %c\n", vitamina);
+	printf("Ah cara, RDR2 nao ganhou o %s \n", "game awards 2018?");
+	printf("boss ta\n");
+	return 0;
+	} 
